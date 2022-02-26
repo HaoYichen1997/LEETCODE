80d4f7f2d099ad0d0f85e7beb27441ec5b24bf15
HYC <hycreator1002@gmail.com>
2022-02-26 02:58:19 -0500

    不太理解，明天重新思考下

---
+++ threesumclosest.py
@@ -0,0 +1,56 @@
+'''
+给你一个长度为 n 的整数数组nums和 一个目标值target。请你从 nums 中选出三个整数，使它们的和与target最接近。
+
+返回这三个数的和。
+
+假定每组输入只存在恰好一个解。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/3sum-closest
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        best = 10 ** 7
+
+        # 根据差值的绝对值来更新答案
+        def update(cur):
+            nonlocal best
+            if abs(cur - target) < abs(best - target):
+                best = cur
+
+        # 枚举 a
+        for i in range(n):
+            # 保证和上一次枚举的元素不相等
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            # 使用双指针枚举 b 和 c
+            j, k = i + 1, n - 1
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                # 如果和为 target 直接返回答案
+                if s == target:
+                    return target
+                update(s)
+                if s > target:
+                    # 如果和大于 target，移动 c 对应的指针
+                    k0 = k - 1
+                    # 移动到下一个不相等的元素
+                    while j < k0 and nums[k0] == nums[k]:
+                        k0 -= 1
+                    k = k0
+                else:
+                    # 如果和小于 target，移动 b 对应的指针
+                    j0 = j + 1
+                    # 移动到下一个不相等的元素
+                    while j0 < k and nums[j0] == nums[j]:
+                        j0 += 1
+                    j = j0
+
+        return best
+
+
