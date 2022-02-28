ff6583ceedd354046b7928646458a67239da34cf
HYC <hycreator1002@gmail.com>
2022-02-27 23:37:51 -0500

    明日一题

---
+++ letterCombinations.py
@@ -0,0 +1,40 @@
+'''
+给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+包含所有组合的题 一般是搜索方法 深度优先和广度优先普遍都是回溯法
+多多考虑嵌套
+
+'''
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        if not digits:
+            return list()
+
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(index: int):
+            if index == len(digits):
+                combinations.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phoneMap[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
+
+        combination = list()
+        combinations = list()
+        backtrack(0)
+        return combinations
