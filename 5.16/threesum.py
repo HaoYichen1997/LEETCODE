dd880e981b86c521b372e463e091ccbdb160dc74
HYC <hycreator1002@gmail.com>
2022-06-27 01:21:39 -0400

    111

--- 5.16/threesum.py
+++ 5.16/threesum.py
@@ -7,14 +7,15 @@
 链接：https://leetcode-cn.com/problems/3sum
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
-import Twosum as twosum
+
+
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         b = list()
         length =len(nums)
         for i in range(length):
             a = list()
-            a = twosum.twosum3(nums[i+1:],-nums[i])
+            a = Twosum.twosum3(nums[i + 1:], -nums[i])
             a.insert(0,i)
             b.append(a)
         return b
